b698d51616038b77b1b5e95c8a50848ab010c786
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-07-13 16:47:44 +0530

    feat: add invisible floor and side walls for physics bounds

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   var W = window.innerWidth,
     H = window.innerHeight;
 
-    var Engine = Matter.Engine,
+  var Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies,
     Body = Matter.Body,
@@ -16,4 +16,33 @@
   var engine = Engine.create();
   engine.gravity.y = 0.9;
   var world = engine.world;
+
+  var wallThickness = 100;
+  var BOTTOM_PADDING = 60;
+  function buildWalls() {
+    var floorTop = H - BOTTOM_PADDING;
+    var floor = Bodies.rectangle(
+      W / 2,
+      floorTop + wallThickness / 2,
+      W * 4,
+      wallThickness,
+      { isStatic: true },
+    );
+    var leftWall = Bodies.rectangle(
+      -wallThickness / 2,
+      H / 2,
+      wallThickness,
+      H * 4,
+      { isStatic: true },
+    );
+    var rightWall = Bodies.rectangle(
+      W + wallThickness / 2,
+      H / 2,
+      wallThickness,
+      H * 4,
+      { isStatic: true },
+    );
+    World.add(world, [floor, leftWall, rightWall]);
+  }
+  buildWalls();
 })();
