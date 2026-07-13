3e5ee37b2ace954fe407e005d57ba7344a06f04f
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-07-13 16:45:45 +0530

    feat: initialize Matter.js engine, world and gravity

--- script.js
+++ script.js
@@ -2,4 +2,18 @@
   var stage = document.getElementById("stage");
   var W = window.innerWidth,
     H = window.innerHeight;
+
+    var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Vertices = Matter.Vertices,
+    Mouse = Matter.Mouse,
+    MouseConstraint = Matter.MouseConstraint,
+    Events = Matter.Events,
+    Runner = Matter.Runner;
+
+  var engine = Engine.create();
+  engine.gravity.y = 0.9;
+  var world = engine.world;
 })();
